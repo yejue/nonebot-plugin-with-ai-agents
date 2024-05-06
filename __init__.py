09c80eab3fce1e37ac781c83e75676e638f38247
yejue <1145331931@qq.com>
2024-05-06 13:42:22 +0800

    [chore] 插件元数据更新

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from nonebot.plugin import PluginMetadata
 
 from .handler import agents
+from .llm.config import Config
 
 __plugin_meta__ = PluginMetadata(
     name="with_ai_agents",
     description="AI 智能体，提供 AI 本身的通用聊天功能与诸如联网、网页内容总结等附加能力",
     usage="无特定用法，聊天即可，可以通过询问功能来查看有哪些用法",
+    config=Config,
 )
