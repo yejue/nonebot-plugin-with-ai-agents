b6986c0b56a3f2c6e59e1d3fe6a721c43679e94f
yejue <1145331931@qq.com>
2024-05-08 17:41:14 +0800

    [chore] 更改响应事件为 on_message

--- handler.py
+++ handler.py
@@ -1,18 +1,15 @@
-from nonebot.plugin import on_command
+from nonebot.plugin import on_message
 from nonebot.rule import to_me
-from nonebot.adapters import Message
-from nonebot.params import CommandArg
+from nonebot.adapters import Event
 
 from .llm import central_brain
 
-agents = on_command("", rule=to_me(), priority=999, block=True)
+agents = on_message(rule=to_me(), priority=999, block=True)
 
 
 @agents.handle()
-async def agent_handler(args: Message = CommandArg()):
+async def agent_handler(event: Event):
     """中央处理器"""
-    if not args:
-        return
-    question = args.extract_plain_text()
+    question = event.get_message().extract_plain_text()
     ans = await central_brain.ask_central_brain(question)
     await agents.finish(ans)
